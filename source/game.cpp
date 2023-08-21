5b581159f6c1bf49d9c2128ae534a5ef2c30f2d2
ThisAster <freizen034@gmail.com>
2023-08-21 06:46:22 +0300

    finish

--- source/game.cpp
+++ source/game.cpp
@@ -7,6 +7,8 @@ bool gameOver;
 const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
 enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
 eDirection dir;
 
@@ -37,9 +39,22 @@ void draw()
                 cout << "#";
             if (i == y && j == x)
                 cout << "0";
-            else if (i == fruitX && j == fruitY)
+            else if (i == fruitY && j == fruitX)
                 cout << "F";
-            else cout << " ";
+            else
+            {
+                bool print = false;
+                for (int k = 0; k < nTail; k++)
+                {
+                    if (tailX[k] == j && tailY[k] == i)
+                    {
+                        print = true;
+                        cout << "o";
+                    }
+                }
+                if (!print)
+                    cout << " ";
+            }
         }
         cout << endl;
     }
@@ -77,7 +92,21 @@ void input()
 
 /* логика всей игры */
 void logic()
-{
+{   
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for (int i = 1; i < nTail; i++)
+    {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
     switch (dir)
     {
     case LEFT:
@@ -94,12 +123,28 @@ void logic()
         break;
     }
 
-    if (x > width || x < 0 || y > height || y < 0)
-        gameOver = true;
+    // if (x > width || x < 0 || y > height || y < 0)
+    //     gameOver = true;
+    if (x >= width - 1)
+        x = 0;
+    else if (x < 0)
+        x = width - 2;
+
+    if (y >= height)
+        y = 0;
+    else if (y < 0)
+        y = height - 1;
+
+    for (int i = 0; i < nTail; i++)
+    {
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+    }
     if (x == fruitX && y == fruitY) {
         score += 10;
         fruitX = rand() % width;
         fruitY = rand() % height;
+        nTail++;
     }
 }
 
